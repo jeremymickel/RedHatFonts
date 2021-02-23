2eeb43c3764cc6becc1750176dbef0be929e937b
Jeremy Mickel <jeremymickel@gmail.com>
2021-02-23 13:59:58 -0800

    fontbakery fixes

--- build-all.sh
+++ build-all.sh
@@ -2,15 +2,15 @@
 
 set -e
 
-if [ -z "$1" ]
-then
-	echo "No version number supplied. If you wish to update the version number in UFOs & built fonts, add one as a build argument:"
-	echo "sources/build-all.sh 1.001"
-else
-	version=$1
-	python mastering/scripts/edit-ufo-info/set-ufo-version.py source/Mono $version --save
-	python mastering/scripts/edit-ufo-info/set-ufo-version.py source/Proportional $version --save
-fi
+# if [ -z "$1" ]
+# then
+# 	echo "No version number supplied. If you wish to update the version number in UFOs & built fonts, add one as a build argument:"
+# 	echo "sources/build-all.sh 1.001"
+# else
+# 	version=$1
+# 	python mastering/scripts/edit-ufo-info/set-ufo-version.py source/Mono $version --save
+# 	python mastering/scripts/edit-ufo-info/set-ufo-version.py source/Proportional $version --save
+# fi
 
 build-scripts/build-vf.sh $version
 
